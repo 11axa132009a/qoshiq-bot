6ba43712aac06c5dad5160c75cc4b1388c62f20e
11axa132009a <11axa132009@gmail.com>
2026-05-09 22:45:16 +0500

    Add files via upload

---
+++ bot.py.py
@@ -0,0 +1,37 @@
+import telebot
+import yt_dlp
+
+TOKEN = "7840357321:AAH2ypOIywFM9Vkr-l11cNdH88TN8N84Ypk"
+
+bot = telebot.TeleBot(TOKEN)
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.reply_to(message, "Qoshiq nomini yubor!")
+
+@bot.message_handler(func=lambda m: True)
+def search_music(message):
+    query = message.text
+
+    ydl_opts = {
+        'quiet': True,
+        'extract_flat': True
+    }
+
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        result = ydl.extract_info(f"ytsearch:{query}", download=False)
+
+        if result['entries']:
+            video = result['entries'][0]
+
+            title = video['title']
+            url = f"https://www.youtube.com/watch?v={video['id']}"
+
+            bot.reply_to(
+                message,
+                f"Topildi!\n\n{title}\n{url}"
+            )
+        else:
+            bot.reply_to(message, "Topilmadi!")
+
+bot.infinity_polling()
